6a1fe78deb2760d9a2825474c85ff89f37c006d3
cezary17 <cezarykujath@gmail.com>
2023-04-22 21:26:48 +0200

    setup done?

---
+++ solutions/CodeforcesCodeforcesRound866Div.2/A_YurasNewName.cpp
@@ -0,0 +1,29 @@
+// time-limit: 1000
+// problem-url: https://codeforces.com/contest/1820/problem/A
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+  cout << 5 << endl;
+  cout << 5 << endl;
+  cout << 1 << endl;
+  cout << 1 << endl;
+  cout << 0 << endl;
+  cout << 3 << endl;
+  cout << 2 << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int tc = 1;
+  // cin >> tc;
+  for (int t = 1; t <= tc; t++) {
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+}
